1326d3b2c9035fd11b4e6fd49bf2a4883935abc3
Rayen ben brahim <129397918+RAYEN311@users.noreply.github.com>
2023-08-04 09:22:21 -0700

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,27 @@ table_puiss.appendChild(create_row())
 button_de.addEventListener('click',()=>{
     table_puiss.removeChild(document.getElementById('SP'))
 })
+const tableI_but = document.getElementById('table-I_but')
+
+var PTIT = 0 ;
+
+tableI_but.addEventListener('click',()=>{
+    var ada = document.getElementById('ada').value; 
+    var ndf = document.getElementById('ndf').value;
+    var cal_d  = document.getElementById('cal_d').value;
+    var nb_com  = document.getElementById('nb_com').value;
+    var surfm2 = document.getElementById('surfm2').value;
+    var adl  = document.getElementById('adl').value;
+    const tableI  = document.getElementById('table_I');
+    PTIT = PTIT + (cal_d * 420 / 1000) ;
+    function create_row_I(){
+    let tr  = document.createElement('tr')
+    tr.innerHTML = '<tr style="background-color:white ;"><td>&nbsp;&nbsp;&nbsp; '+ ada +'  &nbsp;&nbsp;</td><td>&nbsp;  '+ ndf +'*'+cal_d+'A  &nbsp;</td><td>&nbsp;  '+ nb_com +'  &nbsp;</td><td>&nbsp;&nbsp; '+ surfm2 +'  &nbsp;&nbsp;</td><td>&nbsp;  '+ cal_d * 420 / 1000 +'  &nbsp;</td><td>&nbsp;  '+ adl +'  &nbsp;</td></tr>'
+    return tr ;
+    }
+    tableI.appendChild(create_row_I())
+    document.getElementById('ptt').innerText = 'puissance total = ' + PTIT.toString() + ' KW'
+})
 
 var curr_page ;
 var scrol_unit = window.screen.width
